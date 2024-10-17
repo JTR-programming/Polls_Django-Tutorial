2c488bf7d466c5c9f3636670dd643e49a1c57105
JTR-programming <69212793+JTR-programming@users.noreply.github.com>
2024-10-17 18:19:53 -0500

    History and setup files
    Files I used to keep track of terminal commands. --To set it all up and each subsequent terminal command.

---
+++ setup.sh
@@ -0,0 +1,18 @@
+^C(venv) jroberson@LAPTOP-M7J6UPR7:~/src/django-tutorial/mysite$ history
+    1  pip install django
+    2  pip uninstall django
+    3  mkdir django
+    4  mv django django-tutorial
+    5  cd django-tutorial/
+    6  python -m venv venv
+    7  python3 -m venv venv
+    8  sudo apt install python3.10-venv
+    9  python3 -m venv venv
+   10  ls
+   11  source venv/bin/activate  # Activate Virt.Env 
+   12  pip install django
+   13  python manage.py runserver
+   14  cd mysite
+   15  python manage.py runserver
+   16  history
+(venv) jroberson
